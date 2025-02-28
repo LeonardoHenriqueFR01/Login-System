f2522a293238b07937afd6bf56e0ca85492537b5
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-02-28 14:35:19 -0300

    spin button commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, session, Blueprint
 from .models import db, User
+from time import sleep
 
 
 main = Blueprint('main', __name__)
@@ -8,20 +9,19 @@ main = Blueprint('main', __name__)
 # Rota para página principal 
 @main.route('/')
 def home():
+    sleep(3)
     return render_template('index.html')
 
 
 # Rota para página apos fazer cadastro
 @main.route('/sucess_cadastro')
 def sucess_cadastro():
-    name = request.args('name')
     return render_template('sucess_cadastro.html')
 
 
 # Rota para página apos fazer login
 @main.route('/sucess_login')
 def sucess_login():
-    name = request.args('name')
     return render_template('sucess_login.html')
 
 
@@ -36,14 +36,15 @@ def cadastro():
         user_exists = User.query.filter_by(name=name, email=email).first()
 
         if user_exists:
+            return redirect(url_for('main.home', error="Nome ou Email já cadastrado."))            
+        else:
             user = User(name=name, email=email, password=password)
 
             db.session.add(user)
             db.session.commit()
 
+            sleep(3)
             return redirect(url_for('main.sucess_cadastro', name=name))
-        else:
-            return redirect(url_for('main.home', error="Nome ou Email já cadastrado."))
     
     return render_template('index.html')
 
@@ -52,13 +53,14 @@ def cadastro():
 @main.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == "POST":
-        name = request.form.get('name')
-        email = request.form.get('email')
-        password = request.form.get('password')
+        name = request.form.get('name_login')
+        email = request.form.get('email_login')
+        password = request.form.get('password_login')
 
         user_exists = User.query.filter_by(name=name, email=email, password=password).first()
 
         if user_exists:
+            sleep(3)
             return redirect(url_for('main.sucess_login', name=name))
         else:
             return redirect(url_for('main.home', error="Dados inválidos Tente novamente."))
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,11 +1,8 @@
-// Função para o spin
-function get_submit() {
+// Função para o spin form cadastro
+function get_submit_cadastro() {
     // Pegando os "id" nessesários
     let btn_submit_cadastro = document.getElementById('btn_submit_cadastro');
-    let btn_submit_login = document.getElementById('btn_submit_login');
-
     let box_load_cadastro = document.getElementById('box_load_cadastro');
-    let box_load_login = document.getElementById('box_load_login');
 
     let name = document.getElementById('name').value.trim();
     let email = document.getElementById('email').value.trim();
@@ -13,25 +10,58 @@ function get_submit() {
 
     // Validação dos campos
     if (name.length < 3) {
+        alert("O nome precisa ter no mínimo 3 caracteres!")
         return;
     }
 
     if (email.length < 12) {
+        alert("O email precisa ter no mínimo 12 caracteres!")
         return;
     }
 
     if (password.length < 8) {
+        alert("A senha precisa ter no mínimo 8 caracteres!")
         return;
     }
 
     // Se tudo estiver correto, exibe o spin e esconde o botão
-    btn_submit_cadastro.style.display = "none";
-    btn_submit_login.style.display = "none";
-    
+    btn_submit_cadastro.style.display = "none";    
     box_load_cadastro.style.display = "flex";
+
+}
+
+// Função para o spin form login
+function get_submit_login() {
+    // Pegando os "id" nessesários
+    let btn_submit_login = document.getElementById('btn_submit_login');
+    let box_load_login = document.getElementById('box_load_login');
+
+    let name = document.getElementById('name_login').value.trim();
+    let email = document.getElementById('email_login').value.trim();
+    let password = document.getElementById('password_login').value.trim();
+
+    // Validação dos campos
+    if (name.length < 3) {
+        alert("O nome precisa ter no mínimo 3 caracteres!")
+        return;
+    }
+
+    if (email.length < 12) {
+        alert("O email precisa ter no mínimo 12 caracteres!")
+        return;
+    }
+
+    if (password.length < 8) {
+        alert("A senha precisa ter no mínimo 8 caracteres!")
+        return;
+    }
+
+    // Se tudo estiver correto, exibe o spin e esconde o botão
+    btn_submit_login.style.display = "none";
     box_load_login.style.display = "flex";
 
 }
+
 
 function toggleForms() {
     // Seleciona o container principal e alterna a classe "active"
