94a23593fddd9a38b7835a567a0496e5919e3635
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-02-28 05:15:55 -0300

    interface get login/cadastro commit

---
+++ app/static/js/sucess_cadastro.js
@@ -0,0 +1,8 @@
+// Função para chamar o spin
+function btn_return() {
+    let btn_return = document.getElementById('btn_return')   
+    let load = document.getElementById('load')
+
+    btn_return.style.display = "none"
+    load.style.display = "flex"
+}
